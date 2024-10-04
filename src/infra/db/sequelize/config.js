ec0289985070d9626dd17902b6df9197a6172fac
RudsonSantana <rudsonsanttana@gmail.com>
2024-10-04 01:04:33 -0300

    raw: adjustments to configuration files

--- src/infra/db/sequelize/config.js
+++ src/infra/db/sequelize/config.js
@@ -10,17 +10,19 @@ module.exports = {
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.PRODUCTION_DB_USERNAME,
+    password: process.env.PRODUCTION_DB_PASSWORD,
+    database: process.env.PRODUCTION_DB_DATABASE,
+    host: process.env.PRODUCTION_DB_HOST,
+    port: process.env.PRODUCTION_DB_PORT,
     dialect: "mysql"
   }
 }
